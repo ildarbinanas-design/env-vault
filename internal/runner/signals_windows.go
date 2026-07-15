8b5c729762bae7f27f25ef8200711b3e9e6b53b7
Ildar Zaripov <your-email@example.com>
2026-07-16 00:39:12 +0500

    fix: close signal forwarding startup race

--- internal/runner/signals_windows.go
+++ internal/runner/signals_windows.go
@@ -7,9 +7,18 @@ import (
 	"os/signal"
 )
 
-func forwardSignals(process *os.Process) func() {
+func signalNotifications() chan os.Signal {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
+	return ch
+}
+
+func stopSignalNotifications(ch chan os.Signal) {
+	signal.Stop(ch)
+	close(ch)
+}
+
+func forwardSignals(process *os.Process, ch chan os.Signal) func() {
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
@@ -18,8 +27,7 @@ func forwardSignals(process *os.Process) func() {
 		}
 	}()
 	return func() {
-		signal.Stop(ch)
-		close(ch)
+		stopSignalNotifications(ch)
 		<-done
 	}
 }
